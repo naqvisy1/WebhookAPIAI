2a1ba0bf8a153329592c600d07424638ab9a1769
naqvisy1 <naqvisy1@msu.edu>
2017-10-10 01:10:34 -0400

    Update index.js

--- index.js
+++ index.js
@@ -12,10 +12,13 @@ restService.use(bodyParser.urlencoded({
 restService.use(bodyParser.json());
 
 restService.post('/echo', function(req, res) {
-    var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
+    
+    var accountId = "1234";
+    var response = "Your balance is 1000";
+    //var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
     return res.json({
-        speech: speech,
-        displayText: speech,
+        speech: response,
+        displayText: response,
         source: 'webhook-echo-sample'
     });
 });
