f6a7401f296191d4916adff4a07d2392de8ec407
naqvisy1 <naqvisy1@msu.edu>
2017-10-13 02:57:56 -0400

    Update index.js

--- index.js
+++ index.js
@@ -15,47 +15,47 @@ app.use(bodyParser.json());
 
 app.post('/echo', function(req, res) {
 
-    var accountNumber = parseInt(req.body.result.parameters.bankAccountNumber);
-    var response = "";
-    http.post("http://api.msufcuchatbot.me/getBalance/", {accountId: accountNumber, code: “amzn1.ask.account.AGPEDC3Y57INSQR2Z7PPA6V7MV3GVNC6X2ZAEBXAIVP2SFA3VOZNLC537ML6Q5NEBPEQEEBT2AITE62N2OPW6YX37QZATHY7RHNGUDY5PHDADMAC5NBBBWSEFDCJR45VA3KOYDRDTGV5J743SAFSFUZFF7XM6Q3RNQTPMB5G24MFWYWBOSATFP7DIE7XG4BHCEUPKTP3ZRVIBFI”}, (resp) => {
-      let data = '';
+//     var accountNumber = parseInt(req.body.result.parameters.bankAccountNumber);
+//     var response = "";
+//     http.post("http://api.msufcuchatbot.me/getBalance/", {accountId: accountNumber, code: “amzn1.ask.account.AGPEDC3Y57INSQR2Z7PPA6V7MV3GVNC6X2ZAEBXAIVP2SFA3VOZNLC537ML6Q5NEBPEQEEBT2AITE62N2OPW6YX37QZATHY7RHNGUDY5PHDADMAC5NBBBWSEFDCJR45VA3KOYDRDTGV5J743SAFSFUZFF7XM6Q3RNQTPMB5G24MFWYWBOSATFP7DIE7XG4BHCEUPKTP3ZRVIBFI”}, (resp) => {
+//       let data = '';
 
-      // A chunk of data has been recieved.
-      resp.on('data', (chunk) => {
-        data += chunk;
-      });
+//       // A chunk of data has been recieved.
+//       resp.on('data', (chunk) => {
+//         data += chunk;
+//       });
 
-      // The whole response has been received. Print out the result.
-      resp.on('end', () => {
-        console.log(JSON.parse(data));
-        response = JSON.parse(data);
-        res.send(response);
-        return;
-      });
+//       // The whole response has been received. Print out the result.
+//       resp.on('end', () => {
+//         console.log(JSON.parse(data));
+//         response = JSON.parse(data);
+//         res.send(response);
+//         return;
+//       });
 
-    }).on("error", (err) => {
-      console.log("Error: " + err.message);
+//     }).on("error", (err) => {
+//       console.log("Error: " + err.message);
+//     });
+//     console.log("1", response);
+
+    if(accountNumber === 532579)
+    {
+        var response = "Your balance is $3,498.63";
+    }
+    else if(accountNumber === 473823)
+    {
+        var response = "Your balance is $1,856.99";
+    }
+    else
+    {
+        var response = "Sorry, but that account does not exist at this time";
+    }
+    var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
+    return res.json({
+        speech: response,
+        displayText: response,
+        source: 'msufcuchatbot'
     });
-    console.log("1", response);
-
-    // if(accountNumber === 532579)
-    // {
-    //     var response = "Your balance is $3,498.63";
-    // }
-    // else if(accountNumber === 473823)
-    // {
-    //     var response = "Your balance is $1,856.99";
-    // }
-    // else
-    // {
-    //     var response = "Sorry, but that account does not exist at this time";
-    // }
-    //var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
-    // return res.json({
-    //     speech: response,
-    //     displayText: response,
-    //     source: 'msufcuchatbot'
-    // });
 });
 
 app.listen((process.env.PORT || 8000), function() {
