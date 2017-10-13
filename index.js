fc05b2b81a881c596bc4606dcb4e5941d1b8c6ea
naqvisy1 <naqvisy1@msu.edu>
2017-10-13 03:09:26 -0400

    Update index.js

--- index.js
+++ index.js
@@ -16,15 +16,15 @@ app.use(bodyParser.json());
 app.post('/echo', function(req, res) {
     
     var accountNumber = parseInt(req.body.result.parameters.bankAccountNumber);
+    var speech2 = "my account balance is 5000";
     var response = "";
     return res.json({
-            speech: speech,
-            displayText: speech,
+            speech: speech2,
+            displayText: speech2,
             source: 'msufcuchatbot'
         });
     http.post("http://api.msufcuchatbot.me/getBalance/", {accountId: accountNumber, code: "amzn1.ask.account.AGPEDC3Y57INSQR2Z7PPA6V7MV3GVNC6X2ZAEBXAIVP2SFA3VOZNLC537ML6Q5NEBPEQEEBT2AITE62N2OPW6YX37QZATHY7RHNGUDY5PHDADMAC5NBBBWSEFDCJR45VA3KOYDRDTGV5J743SAFSFUZFF7XM6Q3RNQTPMB5G24MFWYWBOSATFP7DIE7XG4BHCEUPKTP3ZRVIBFI"}, (resp) => {
       let data = '';
-        var speech = "my account balance is 5000";
       // A chunk of data has been recieved.
       resp.on('data', (chunk) => {
         data += chunk;
