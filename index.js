1f3e8b4e70e795f5e4cd80e224531fe7947ef3b0
naqvisy1 <naqvisy1@msu.edu>
2017-10-13 03:45:39 -0400

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const app = express();
 
 const http = require('http');
+var request = require('request');
 
 app.use(bodyParser.urlencoded({
     extended: true
@@ -19,12 +20,28 @@ app.post('/echo', function(req, res) {
     var speech2 = "my account balance is 5000";
     var response = "";
 
-    http.post("http://api.msufcuchatbot.me/getBalance/", {accountId: accountNumber, code: "amzn1.ask.account.AGPEDC3Y57INSQR2Z7PPA6V7MV3GVNC6X2ZAEBXAIVP2SFA3VOZNLC537ML6Q5NEBPEQEEBT2AITE62N2OPW6YX37QZATHY7RHNGUDY5PHDADMAC5NBBBWSEFDCJR45VA3KOYDRDTGV5J743SAFSFUZFF7XM6Q3RNQTPMB5G24MFWYWBOSATFP7DIE7XG4BHCEUPKTP3ZRVIBFI"}, (resp) => {
+request.post(
+    'http://api.msufcuchatbot.me/getBalance/',
+    { json: {accountId: accountNumber, code: "amzn1.ask.account.AGPEDC3Y57INSQR2Z7PPA6V7MV3GVNC6X2ZAEBXAIVP2SFA3VOZNLC537ML6Q5NEBPEQEEBT2AITE62N2OPW6YX37QZATHY7RHNGUDY5PHDADMAC5NBBBWSEFDCJR45VA3KOYDRDTGV5J743SAFSFUZFF7XM6Q3RNQTPMB5G24MFWYWBOSATFP7DIE7XG4BHCEUPKTP3ZRVIBFI"} },
+    function (error, response, body) {
+        console.log("requestasdsfasf");
+        if (!error && response.statusCode == 200) {
+            console.log(body)
+        }
+        console.log("yo");
         return res.json({
             speech: speech2,
             displayText: speech2,
             source: 'msufcuchatbot'
         });
+    }
+);
+//     http.post("http://api.msufcuchatbot.me/getBalance/", {accountId: accountNumber, code: "amzn1.ask.account.AGPEDC3Y57INSQR2Z7PPA6V7MV3GVNC6X2ZAEBXAIVP2SFA3VOZNLC537ML6Q5NEBPEQEEBT2AITE62N2OPW6YX37QZATHY7RHNGUDY5PHDADMAC5NBBBWSEFDCJR45VA3KOYDRDTGV5J743SAFSFUZFF7XM6Q3RNQTPMB5G24MFWYWBOSATFP7DIE7XG4BHCEUPKTP3ZRVIBFI"}, (resp) => {
+//         return res.json({
+//             speech: speech2,
+//             displayText: speech2,
+//             source: 'msufcuchatbot'
+//         });
         //       let data = '';
 //       // A chunk of data has been recieved.
 //       resp.on('data', (chunk) => {
