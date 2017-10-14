5fc27f45baf68d4bb77f89a89e9bdbb7951fa663
naqvisy1 <naqvisy1@msu.edu>
2017-10-14 00:30:05 -0400

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ app.post('/echo', function(req, res) {
     
     var accountNumber = parseInt(req.body.result.parameters.bankAccountNumber);
     var response = "";
-    var final = "";
 
 request.post(
     'https://api.msufcuchatbot.me/getBalance/',
@@ -30,9 +29,8 @@ request.post(
             displayText: response.statusCode,
             source: 'msufcuchatbot'
         });
-     });
-     }
-  }
+        }
+    }
 );
 //     http.post("http://api.msufcuchatbot.me/getBalance/", {accountId: accountNumber, code: "amzn1.ask.account.AGPEDC3Y57INSQR2Z7PPA6V7MV3GVNC6X2ZAEBXAIVP2SFA3VOZNLC537ML6Q5NEBPEQEEBT2AITE62N2OPW6YX37QZATHY7RHNGUDY5PHDADMAC5NBBBWSEFDCJR45VA3KOYDRDTGV5J743SAFSFUZFF7XM6Q3RNQTPMB5G24MFWYWBOSATFP7DIE7XG4BHCEUPKTP3ZRVIBFI"}, (resp) => {
 //         return res.json({
