64f32f58b17e7cd68c31120c85be93ec5fd88f3e
naqvisy1 <naqvisy1@msu.edu>
2017-10-14 00:18:25 -0400

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ request.post(
     { json: {"accountId": accountNumber, "code": "amzn1.ask.account.AGPEDC3Y57INSQR2Z7PPA6V7MV3GVNC6X2ZAEBXAIVP2SFA3VOZNLC537ML6Q5NEBPEQEEBT2AITE62N2OPW6YX37QZATHY7RHNGUDY5PHDADMAC5NBBBWSEFDCJR45VA3KOYDRDTGV5J743SAFSFUZFF7XM6Q3RNQTPMB5G24MFWYWBOSATFP7DIE7XG4BHCEUPKTP3ZRVIBFI"} },
     function (error, response) {
         if (!error && response.statusCode == 200) {
-            var final = response;
+            final = response;
         });
         }
     }
