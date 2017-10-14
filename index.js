94faab84025b4612472e22e4ae314c5151ab7b17
naqvisy1 <naqvisy1@msu.edu>
2017-10-14 00:40:59 -0400

    Update index.js

--- index.js
+++ index.js
@@ -25,16 +25,14 @@ request.post(
     { json: {"accountId": accountNumber, "code": "amzn1.ask.account.AGPEDC3Y57INSQR2Z7PPA6V7MV3GVNC6X2ZAEBXAIVP2SFA3VOZNLC537ML6Q5NEBPEQEEBT2AITE62N2OPW6YX37QZATHY7RHNGUDY5PHDADMAC5NBBBWSEFDCJR45VA3KOYDRDTGV5J743SAFSFUZFF7XM6Q3RNQTPMB5G24MFWYWBOSATFP7DIE7XG4BHCEUPKTP3ZRVIBFI"} },
     function (error, response) {
         if (!error && response.statusCode == 200) {
-            final = response
+            return res.json({
+            speech: "blah" + response,
+            displayText: "blah" + response,
+            source: 'msufcuchatbot'
+        });
         }
     }
 );
-    
-            return res.json({
-            speech: final,
-            displayText: final,
-            source: 'msufcuchatbot'
-        });
 //     http.post("http://api.msufcuchatbot.me/getBalance/", {accountId: accountNumber, code: "amzn1.ask.account.AGPEDC3Y57INSQR2Z7PPA6V7MV3GVNC6X2ZAEBXAIVP2SFA3VOZNLC537ML6Q5NEBPEQEEBT2AITE62N2OPW6YX37QZATHY7RHNGUDY5PHDADMAC5NBBBWSEFDCJR45VA3KOYDRDTGV5J743SAFSFUZFF7XM6Q3RNQTPMB5G24MFWYWBOSATFP7DIE7XG4BHCEUPKTP3ZRVIBFI"}, (resp) => {
 //         return res.json({
 //             speech: speech2,
