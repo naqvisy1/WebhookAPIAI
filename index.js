47d291c8aca2e1c36244ffa0f87ededef9b80ebc
naqvisy1 <naqvisy1@msu.edu>
2017-10-14 01:15:30 -0400

    Update index.js

--- index.js
+++ index.js
@@ -26,8 +26,8 @@ request.post(
     function (error, response) {
         if (!error && response.statusCode == 200) {
             return res.json({
-            speech: "blah" + response,
-            displayText: "blah" + response,
+            speech: "blah" + JSON.stringify(response),
+            displayText: "blah" + JSON.stringify(response),
             source: 'msufcuchatbot'
         });
         }
