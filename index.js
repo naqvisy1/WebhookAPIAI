59633df782a9f61940bc71681d088518cc9bf3a1
naqvisy1 <naqvisy1@msu.edu>
2017-10-13 23:33:30 -0400

    Update index.js

--- index.js
+++ index.js
@@ -20,13 +20,14 @@ app.post('/echo', function(req, res) {
     var response = "";
 
 request.post(
-    'https://api.msufcuchatbot.me/test/',
+    'https://api.msufcuchatbot.me/getBalance/',
+    { json: {"accountId": accountNumber, "code": "amzn1.ask.account.AGPEDC3Y57INSQR2Z7PPA6V7MV3GVNC6X2ZAEBXAIVP2SFA3VOZNLC537ML6Q5NEBPEQEEBT2AITE62N2OPW6YX37QZATHY7RHNGUDY5PHDADMAC5NBBBWSEFDCJR45VA3KOYDRDTGV5J743SAFSFUZFF7XM6Q3RNQTPMB5G24MFWYWBOSATFP7DIE7XG4BHCEUPKTP3ZRVIBFI"} },
     function (error, response) {
         if (!error && response.statusCode == 200) {
             return res.json({
-                speech: "test",
-                displayText: "test",
-                source: 'msufcuchatbot'
+            speech: "blah",
+            displayText: "blah",
+            source: 'msufcuchatbot'
         });
         }
     }
