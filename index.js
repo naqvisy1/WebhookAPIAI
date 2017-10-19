d2ecb6af118f81f9a11c6c60175679f96ad0a2ee
Josh Benner <bennerster@gmail.com>
2017-10-19 14:52:43 -0400

    Update index.js

--- index.js
+++ index.js
@@ -35,11 +35,19 @@ app.post('/echo', function(req, res) {
             }
         );
     } else {
-        return res.json({
-                        speech: "Stuff"),
-                        displayText: "Stuff",
+        request.post(
+            'https://api.msufcuchatbot.me/transferMoney/',
+            { json: {"accountId1": 9999, "accountId2": 9876, "amount":500, "code": "amzn1.ask.account.AGPEDC3Y57INSQR2Z7PPA6V7MV3GVNC6X2ZAEBXAIVP2SFA3VOZNLC537ML6Q5NEBPEQEEBT2AITE62N2OPW6YX37QZATHY7RHNGUDY5PHDADMAC5NBBBWSEFDCJR45VA3KOYDRDTGV5J743SAFSFUZFF7XM6Q3RNQTPMB5G24MFWYWBOSATFP7DIE7XG4BHCEUPKTP3ZRVIBFI"} },
+            function (error, response) {
+                if (!error && response.statusCode == 200) {
+                    return res.json({
+                        speech: "Your account balance is " + JSON.stringify(response.body.balance),
+                        displayText: "Your account balance is " + JSON.stringify(response.body.balance),
                         source: 'msufcuchatbot'
                     });
+                }
+            }
+        );
     }
 
 
