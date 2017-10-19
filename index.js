57b7da515f39a0e3ed4387c44471d0f53f39c1ef
Josh Benner <bennerster@gmail.com>
2017-10-19 14:35:42 -0400

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 
-app.post('/dialogflow', function(req, res) {
+app.post('/echo', function(req, res) {
     return res.json({
                         speech: JSON.stringify(response.body),
                         displayText: JSON.stringify(response.body),
