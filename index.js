6be357724cac3bfe3706ad9207e935d97ac67306
Josh Benner <bennerster@gmail.com>
2017-10-19 15:14:27 -0400

    Update index.js

--- index.js
+++ index.js
@@ -37,10 +37,12 @@ app.post('/echo', function(req, res) {
             }
         );
     } else {
+        var accountNumber1 = parseInt(req.body.result.parameters.bankAccountNumber1);
+        var accountNumber2 = parseInt(req.body.result.parameters.bankAccountNumber2);
         var amount = parseInt(req.body.result.parameters.amount);
         request.post(
             'https://api.msufcuchatbot.me/transferMoney/',
-            { json: {"accountId1": 9999, "accountId2": 9876, "amount":amount, "code": "amzn1.ask.account.AGPEDC3Y57INSQR2Z7PPA6V7MV3GVNC6X2ZAEBXAIVP2SFA3VOZNLC537ML6Q5NEBPEQEEBT2AITE62N2OPW6YX37QZATHY7RHNGUDY5PHDADMAC5NBBBWSEFDCJR45VA3KOYDRDTGV5J743SAFSFUZFF7XM6Q3RNQTPMB5G24MFWYWBOSATFP7DIE7XG4BHCEUPKTP3ZRVIBFI"} },
+            { json: {"accountId1": accountNumber1, "accountId2": accountNumber2, "amount":amount, "code": "amzn1.ask.account.AGPEDC3Y57INSQR2Z7PPA6V7MV3GVNC6X2ZAEBXAIVP2SFA3VOZNLC537ML6Q5NEBPEQEEBT2AITE62N2OPW6YX37QZATHY7RHNGUDY5PHDADMAC5NBBBWSEFDCJR45VA3KOYDRDTGV5J743SAFSFUZFF7XM6Q3RNQTPMB5G24MFWYWBOSATFP7DIE7XG4BHCEUPKTP3ZRVIBFI"} },
             function (error, response) {
                 if (!error && response.statusCode == 200) {
                     return res.json({
