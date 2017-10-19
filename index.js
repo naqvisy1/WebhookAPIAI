39f4e618b904d13b839c4aa11de8ba3578653da1
Josh Benner <bennerster@gmail.com>
2017-10-19 11:29:43 -0400

    Hard coding account numbers on Transfer Money

--- index.js
+++ index.js
@@ -14,6 +14,29 @@ app.use(bodyParser.urlencoded({
 
 app.use(bodyParser.json());
 
+app.post('/transferMoney', function(req, res) {
+    
+    var accountNumber = parseInt(req.body.result.parameters.bankAccountNumber);
+    var accountNumberToTransferTo = parseInt(req.body.result.parameters.bankAccountNumber2);
+    var amount = parseInt(req.body.result.parameters.amount);
+    var response = "";
+    var final = ""
+
+    request.post(
+        'https://api.msufcuchatbot.me/transferMoney/',
+        { json: {"accountId1": 1234, "accountId2": 9867, "amount": amount, "code": "amzn1.ask.account.AGPEDC3Y57INSQR2Z7PPA6V7MV3GVNC6X2ZAEBXAIVP2SFA3VOZNLC537ML6Q5NEBPEQEEBT2AITE62N2OPW6YX37QZATHY7RHNGUDY5PHDADMAC5NBBBWSEFDCJR45VA3KOYDRDTGV5J743SAFSFUZFF7XM6Q3RNQTPMB5G24MFWYWBOSATFP7DIE7XG4BHCEUPKTP3ZRVIBFI"} },
+        function (error, response) {
+            if (!error && response.statusCode == 200) {
+                return res.json({
+                speech: "Sucessfully transferred " + JSON.stringify(response.body.amount),
+                displayText: "Sucessfully transferred " + JSON.stringify(response.body.amount),
+                source: 'msufcuchatbot'
+            });
+            }
+        }
+    );
+});
+
 app.post('/echo', function(req, res) {
     
     var accountNumber = parseInt(req.body.result.parameters.bankAccountNumber);
@@ -87,29 +110,6 @@ request.post(
 //         displayText: response,
 //         source: 'msufcuchatbot'
 //     });
-});
-
-app.post('/transferMoney', function(req, res) {
-    
-    var accountNumber = parseInt(req.body.result.parameters.bankAccountNumber);
-    var accountNumberToTransferTo = parseInt(req.body.result.parameters.bankAccountNumber2);
-    var amount = parseInt(req.body.result.parameters.amount);
-    var response = "";
-    var final = ""
-
-    request.post(
-        'https://api.msufcuchatbot.me/transferMoney/',
-        { json: {"accountId1": accountNumber, "accountId2": accountNumberToTransferTo, "amount": amount, "code": "amzn1.ask.account.AGPEDC3Y57INSQR2Z7PPA6V7MV3GVNC6X2ZAEBXAIVP2SFA3VOZNLC537ML6Q5NEBPEQEEBT2AITE62N2OPW6YX37QZATHY7RHNGUDY5PHDADMAC5NBBBWSEFDCJR45VA3KOYDRDTGV5J743SAFSFUZFF7XM6Q3RNQTPMB5G24MFWYWBOSATFP7DIE7XG4BHCEUPKTP3ZRVIBFI"} },
-        function (error, response) {
-            if (!error && response.statusCode == 200) {
-                return res.json({
-                speech: "Sucessfully transferred " + JSON.stringify(response.body.amount),
-                displayText: "Sucessfully transferred " + JSON.stringify(response.body.amount),
-                source: 'msufcuchatbot'
-            });
-            }
-        }
-    );
 });
 
 app.listen((process.env.PORT || 8000), function() {
