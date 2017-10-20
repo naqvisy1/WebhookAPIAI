2ab7c29ba1d519edcb734565b517dc3e1ca9b006
Josh Benner <bennerster@gmail.com>
2017-10-20 16:10:39 -0400

    Adding speech back

--- index.js
+++ index.js
@@ -40,6 +40,7 @@ app.post('/echo', function(req, res) {
     }
     else if(req.body.result.action == "debug"){
              return res.json({
+                speech: JSON.stringify(req.body.originalRequest),
                 displayText: JSON.stringify(req.body.originalRequest),
                 source: 'msufcuchatbot'
             });
