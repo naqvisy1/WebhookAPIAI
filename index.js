3fddef15b9e11a2bda69a6f8a741161d43562a52
Josh Benner <bennerster@gmail.com>
2017-10-20 14:26:24 -0400

    Update index.js

--- index.js
+++ index.js
@@ -24,22 +24,22 @@ app.post('/echo', function(req, res) {
             var accountNumber = parseInt(req.body.result.parameters.bankAccountNumber);
             return res.json({
                         speech: JSON.stringify(response.data),
-                        displayText: "JSON.stringify(response.body),
+                        displayText: "JSON.stringify(response.data),
                         source: 'msufcuchatbot'
                     });
-       request.post(
-            'https://api.msufcuchatbot.me/getBalance/',
-            { json: {"accountId": accountNumber, "code": "amzn1.ask.account.AGPEDC3Y57INSQR2Z7PPA6V7MV3GVNC6X2ZAEBXAIVP2SFA3VOZNLC537ML6Q5NEBPEQEEBT2AITE62N2OPW6YX37QZATHY7RHNGUDY5PHDADMAC5NBBBWSEFDCJR45VA3KOYDRDTGV5J743SAFSFUZFF7XM6Q3RNQTPMB5G24MFWYWBOSATFP7DIE7XG4BHCEUPKTP3ZRVIBFI"} },
-            function (error, response) {
-                if (!error && response.statusCode == 200) {
-                    return res.json({
-                        speech: "Your account balance is " + JSON.stringify(response.body.balance),
-                        displayText: "Your account balance is " + JSON.stringify(response.body.balance),
-                        source: 'msufcuchatbot'
-                    });
-                }
-            }
-        );
+//        request.post(
+//             'https://api.msufcuchatbot.me/getBalance/',
+//             { json: {"accountId": accountNumber, "code": "amzn1.ask.account.AGPEDC3Y57INSQR2Z7PPA6V7MV3GVNC6X2ZAEBXAIVP2SFA3VOZNLC537ML6Q5NEBPEQEEBT2AITE62N2OPW6YX37QZATHY7RHNGUDY5PHDADMAC5NBBBWSEFDCJR45VA3KOYDRDTGV5J743SAFSFUZFF7XM6Q3RNQTPMB5G24MFWYWBOSATFP7DIE7XG4BHCEUPKTP3ZRVIBFI"} },
+//             function (error, response) {
+//                 if (!error && response.statusCode == 200) {
+//                     return res.json({
+//                         speech: "Your account balance is " + JSON.stringify(response.body.balance),
+//                         displayText: "Your account balance is " + JSON.stringify(response.body.balance),
+//                         source: 'msufcuchatbot'
+//                     });
+//                 }
+//             }
+//         );
     } else {
         var accountNumber1 = parseInt(req.body.result.parameters.bankAccountNumber1);
         var accountNumber2 = parseInt(req.body.result.parameters.bankAccountNumber2);
