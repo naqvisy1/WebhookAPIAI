83d5b0b49485b27e8880416e3cd1653ad8466feb
Josh Benner <bennerster@gmail.com>
2017-10-20 17:23:12 -0400

    Updating for source/destination

--- index.js
+++ index.js
@@ -21,9 +21,7 @@ app.post('/echo', function(req, res) {
     var response = "";
     var final = ""
     if(req.body.result.action == "echo"){
-        var accountNumber = parseInt(req.body.result.parameters.bankAccountNumber);
-
-
+        var accountNumber = parseInt(req.body.result.parameters.sourceAccountNumber);
        request.post(
             'https://api.msufcuchatbot.me/getBalance/',
             { json: {"accountId": accountNumber, "code": "amzn1.ask.account.AGPEDC3Y57INSQR2Z7PPA6V7MV3GVNC6X2ZAEBXAIVP2SFA3VOZNLC537ML6Q5NEBPEQEEBT2AITE62N2OPW6YX37QZATHY7RHNGUDY5PHDADMAC5NBBBWSEFDCJR45VA3KOYDRDTGV5J743SAFSFUZFF7XM6Q3RNQTPMB5G24MFWYWBOSATFP7DIE7XG4BHCEUPKTP3ZRVIBFI"} },
@@ -70,8 +68,8 @@ app.post('/echo', function(req, res) {
       }
 
     } else {
-        var accountNumber1 = parseInt(req.body.result.parameters.bankAccountNumber1);
-        var accountNumber2 = parseInt(req.body.result.parameters.bankAccountNumber2);
+        var accountNumber1 = parseInt(req.body.result.parameters.sourceAccountNumber);
+        var accountNumber2 = parseInt(req.body.result.parameters.destinationAccountNumber);
         var amount = parseInt(req.body.result.parameters.amount);
         request.post(
             'https://api.msufcuchatbot.me/transferMoney/',
