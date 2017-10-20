509cc901b051780de8dd0f1374c806751b210515
Josh Benner <bennerster@gmail.com>
2017-10-20 14:23:26 -0400

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,11 @@ app.post('/echo', function(req, res) {
     var final = ""
     if(req.body.result.action == "echo"){
             var accountNumber = parseInt(req.body.result.parameters.bankAccountNumber);
-
+            return res.json({
+                        speech: JSON.stringify(response.data),
+                        displayText: "JSON.stringify(response.body),
+                        source: 'msufcuchatbot'
+                    });
        request.post(
             'https://api.msufcuchatbot.me/getBalance/',
             { json: {"accountId": accountNumber, "code": "amzn1.ask.account.AGPEDC3Y57INSQR2Z7PPA6V7MV3GVNC6X2ZAEBXAIVP2SFA3VOZNLC537ML6Q5NEBPEQEEBT2AITE62N2OPW6YX37QZATHY7RHNGUDY5PHDADMAC5NBBBWSEFDCJR45VA3KOYDRDTGV5J743SAFSFUZFF7XM6Q3RNQTPMB5G24MFWYWBOSATFP7DIE7XG4BHCEUPKTP3ZRVIBFI"} },
