b7405553175c2967593358879cdbc6ea287167ea
ctymoszek <31750745+ctymoszek@users.noreply.github.com>
2017-11-05 19:20:13 -0500

    Added autopay and loan functions to webhook

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.post('/echo', function(req, res) {
             });
     }
     else if (req.body.result.action == "UpdateInfo.UpdateInfo-custom") {
-
+ww
       //Update user account info intent
       if (req.body.result.parameters) {
         //Parameters for changing account info
@@ -89,34 +89,7 @@ app.post('/echo', function(req, res) {
             }
         );
     }
-    else if(req.body.result.action == "autopay-setup"){
-      var accountNumber = req.body.result.parameters.accountNumber;
-      var autopayShare = req.body.result.parameters.autopayShare;
-      var sourceShare = req.body.result.parameters.sourceShare;
-      var autopayAmount = req.body.result.parameters.autopayAmount;
-
-      request.post('https://api.msufcuchatbot.me/autopaySetup/',
-        {json: {"accountNumber": accountNumber, "autopayShare": autopayShare,
-          "sourceShare": sourceShare, "autopayAmount": autopayAmount}},
-          function(error, response){
-            console.log("Is this thing on?");
-            if (!error && response.statusCode == 200) {
-              return res.json({speech: "Okay, starting automatic payments. On the "
-                  + response.body.autopayDate
-                  + " of every month, $" + response.body.autopayAmount
-                  + " will be paid to your " + response.body.autopayShare
-                  + " from your " + response.body.sourceShare + ". Is this correct?",
-                displayText: "Okay, starting automatic payments. On the "
-                  + response.body.autopayDate + " of every month, $"
-                  + response.body.autopayAmount + " will be paid to your "
-                  + response.body.autopayShare + " from your "
-                  + response.body.sourceShare + ". Is this correct?",
-                source: 'msufcuchatbot'
-              });
-            }
-          }
-      );
-    } else if (req.body.result.action == "password-reset") {
+    else if (req.body.result.action == "password-reset") {
 
       //Update user account info intent
       if (req.body.result.parameters) {
@@ -141,7 +114,34 @@ app.post('/echo', function(req, res) {
       }
 
     }
-    else if(req.body.result.action == "autopay-setup-intent.autopay-setup-intent-yes"){
+    else if(req.body.result.action == "autopay-setup"){
+      var accountNumber = req.body.result.parameters.accountNumber;
+      var autopayShare = req.body.result.parameters.autopayShare;
+      var sourceShare = req.body.result.parameters.sourceShare;
+      var autopayAmount = req.body.result.parameters.autopayAmount;
+
+      request.post('https://api.msufcuchatbot.me/autopaySetup/',
+        {json: {"accountNumber": accountNumber, "autopayShare": autopayShare,
+          "sourceShare": sourceShare, "autopayAmount": autopayAmount}},
+          function(error, response){
+            if (!error && response.statusCode == 200) {
+              return res.json({speech: "Okay, starting automatic payments. On the "
+                  + response.body.autopayDate
+                  + " of every month, $" + response.body.autopayAmount
+                  + " will be paid to your " + response.body.autopayShare
+                  + " from your " + response.body.sourceShare + ". Is this correct?",
+                displayText: "Okay, starting automatic payments. On the "
+                  + response.body.autopayDate + " of every month, $"
+                  + response.body.autopayAmount + " will be paid to your "
+                  + response.body.autopayShare + " from your "
+                  + response.body.sourceShare + ". Is this correct?",
+                source: 'msufcuchatbot'
+              });
+            }
+          }
+      );
+    }
+    else if(req.body.result.action == "autopay-setup-yes"){
       var accountNumber = req.body.result.parameters.accountNumber;
       var autopayShare = req.body.result.parameters.autopayShare;
       var sourceShare = req.body.result.parameters.sourceShare;
@@ -153,6 +153,136 @@ app.post('/echo', function(req, res) {
             if (!error && response.statusCode == 200) {
               return res.json({speech: "Alright, your automatic payments have been set up. Thanks!",
                 displayText: "Alright, your automatic payments have been set up. Thanks!",
+                source: 'msufcuchatbot'
+              });
+            }
+          }
+      );
+    }
+    else if(req.body.result.action == "autopay-cancel"){
+      var accountNumber = req.body.result.parameters.accountNumber;
+      var autopayShare = req.body.result.parameters.autopayShare;
+      request.post('https://api.msufcuchatbot.me/autopayCancel/',
+        {json: {"accountNumber": accountNumber, "autopayShare": autopayShare}},
+          function(error, response){
+            if (!error && response.statusCode == 200) {
+              return res.json({speech: "Okay, canceling automatic payments for your "
+                  + response.body.autopayShare + ". Is this correct?",
+                displayText: "Okay, canceling automatic payments for your "
+                    + response.body.autopayShare + ". Is this correct?",
+                source: 'msufcuchatbot'
+              });
+            }
+          }
+      );
+    }
+    else if(req.body.result.action == "autopay-cancel-yes"){
+      var accountNumber = req.body.result.parameters.accountNumber;
+      var autopayShare = req.body.result.parameters.autopayShare;
+      var sourceShare = req.body.result.parameters.sourceShare;
+      var autopayAmount = req.body.result.parameters.autopayAmount;
+      request.post('https://api.msufcuchatbot.me/autopayCancelYes/',
+        {json: {"accountNumber": accountNumber, "autopayShare": autopayShare,
+          "sourceShare": sourceShare, "autopayAmount": autopayAmount}},
+          function(error, response){
+            if (!error && response.statusCode == 200) {
+              return res.json({speech: "Alright, your automatic payments have been canceled.",
+                displayText: "Alright, your automatic payments have been canceled.",
+                source: 'msufcuchatbot'
+              });
+            }
+          }
+      );
+    }
+    else if(req.body.result.action == "loan-payment"){
+      var accountNumber = req.body.result.parameters.accountNumber;
+      var loanShare = req.body.result.parameters.loanShare;
+      var sourceShare = req.body.result.parameters.sourceShare;
+      var paymentAmount = req.body.result.parameters.paymentAmount;
+      request.post('https://api.msufcuchatbot.me/loanPayment/',
+        {json: {"accountNumber": accountNumber, "loanShare": loanShare,
+          "sourceShare": sourceShare, "paymentAmount": paymentAmount}},
+          function(error, response){
+            if (!error && response.statusCode == 200) {
+              return res.json({speech: "Okay, making a loan payment. $" + response.body.autopayAmount
+                  + " will be paid to your " + response.body.autopayShare
+                  + " from your " + response.body.sourceShare + ". Is this correct?",
+                displayText: "Okay, making a loan payment. $" + response.body.autopayAmount
+                    + " will be paid to your " + response.body.autopayShare
+                    + " from your " + response.body.sourceShare + ". Is this correct?",
+                source: 'msufcuchatbot'
+              });
+            }
+          }
+      );
+    }
+    else if(req.body.result.action == "loan-payment-yes"){
+      var accountNumber = req.body.result.parameters.accountNumber;
+      var loanShare = req.body.result.parameters.loanShare;
+      var sourceShare = req.body.result.parameters.sourceShare;
+      var paymentAmount = req.body.result.parameters.paymentAmount;
+      request.post('https://api.msufcuchatbot.me/loanPaymentYes/',
+        {json: {"accountNumber": accountNumber, "loanShare": loanShare,
+          "sourceShare": sourceShare, "paymentAmount": paymentAmount}},
+          function(error, response){
+            if (!error && response.statusCode == 200) {
+              return res.json({speech: "Alright, your loan payment has been applied. Thanks!",
+                displayText: "Alright, your loan payment has been applied. Thanks!",
+                source: 'msufcuchatbot'
+              });
+            }
+          }
+      );
+    }
+    else if(req.body.result.action == "get-due-date"){
+      var accountNumber = req.body.result.parameters.accountNumber;
+      var loanShare = req.body.result.parameters.loanShare;
+      request.post('https://api.msufcuchatbot.me/getDueDate/',
+        {json: {"accountNumber": accountNumber, "loanShare": loanShare}},
+          function(error, response){
+            if (!error && response.statusCode == 200) {
+              return res.json({speech: "Your current due date for your "
+                + loanShare + "is the " + response.body.dueDate + " of the month.",
+                displayText: "Your current due date for your " + loanShare
+                + "is the " + response.body.dueDate + " of the month.",
+                source: 'msufcuchatbot'
+              });
+            }
+          }
+      );
+    }
+    else if(req.body.result.action == "change-due-date"){
+      var accountNumber = req.body.result.parameters.accountNumber;
+      var loanShare = req.body.result.parameters.loanShare;
+      var newDate = req.body.result.parameters.newDate;
+      request.post('https://api.msufcuchatbot.me/changeDueDate/',
+        {json: {"accountNumber": accountNumber, "loanShare": loanShare,
+          "newDate": newDate}},
+          function(error, response){
+            if (!error && response.statusCode == 200) {
+              return res.json({speech: "Alright, changing the due date for your "
+                + loanShare + " from the " + response.body.oldDate + " to the "
+                + newDate ". Is this correct?",
+                displayText: "Alright, changing the due date for your "
+                + loanShare + " from the " + response.body.oldDate + " to the "
+                + newDate ". Is this correct?",
+                source: 'msufcuchatbot'
+              });
+            }
+          }
+      );
+    }
+    else if(req.body.result.action == "change-due-date-yes"){
+      var accountNumber = req.body.result.parameters.accountNumber;
+      var loanShare = req.body.result.parameters.loanShare;
+      var newDate = req.body.result.parameters.newDate;
+      request.post('https://api.msufcuchatbot.me/changeDueDateYes/',
+      {json: {"accountNumber": accountNumber, "loanShare": loanShare,
+        "newDate": newDate}},
+          function(error, response){
+            if (!error && response.statusCode == 200) {
+              return res.json({speech: "Alright, your due date has been changed.",
+                displayText: "Alright, your due date has been changed.",
                 source: 'msufcuchatbot'
               });
             }
